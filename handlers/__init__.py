a21b70bb0bb33ab0b6e081a420eec63f92ec8dbe
danyducky <morikov2000@gmail.com>
2020-12-21 02:01:56 +0500

    edited structure, bot_commands

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,3 +1,4 @@
 from .handlers import dp
 
+
 # __all__ = ["dp"]
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from bot import bot, storage
+#from bot import bot, storage
+from filters.bot_commands import default_commands
+
+async def startup(dp):
+
+    await default_commands(dp)
 
 
 if __name__ == '__main__':
     from aiogram import executor
     from handlers import dp
 
-    executor.start_polling(dp, skip_updates = True)
+    executor.start_polling(dp, on_startup=startup, skip_updates = True)
